52885040d385016fb926122b0f9795f58946612a
Octavio Rodríguez Castañeda <octavio.rdz@outlook.com>
2023-02-05 16:11:35 -0700

    Update WindowsAppSDK. Updates to WinUI output content and clean code.

--- src/Xfixy.WinUI/MainWindow.xaml.cs
+++ src/Xfixy.WinUI/MainWindow.xaml.cs
@@ -4,17 +4,9 @@
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using WinRT.Interop;
-
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using Windows.UI.WebUI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -38,150 +30,6 @@ namespace Xfixy.WinUI
             return MsgDateTime.ToString() + " " + MsgText;
         }
     }
-    // https://github.com/AvaloniaUI/Avalonia/discussions/7596
-    /// <summary>
-    ///     Represents a first-in, first-out collection of objects, and supports observation
-    ///     notifications and a max buffer size.
-    /// </summary>
-    /// <typeparam name="T"> The Type to wrap. </typeparam>
-    public class ObservableQueue<T> : Queue<T>, INotifyCollectionChanged, INotifyPropertyChanged
-    {
-        private int bufferSize = -1;
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ObservableQueue{T}" /> class that is
-        ///     empty and has the default initial capacity.
-        /// </summary>
-        public ObservableQueue()
-            : base()
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ObservableQueue{T}" /> class that
-        ///     contains elements copied from the specified collection and has sufficient capacity
-        ///     to accommodate the number of elements copied.
-        /// </summary>
-        public ObservableQueue(IEnumerable<T> collection)
-            : base(collection)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="ObservableQueue{T}" /> class that is
-        ///     empty and has the specified initial capacity. Also sets the buffer to the specified capacity.
-        /// </summary>
-        public ObservableQueue(int capacity)
-            : base(capacity)
-        {
-            this.BufferSize = capacity;
-        }
-
-        /// <summary>
-        ///     Gets or sets the maximum number of elements allowed in the queue.
-        /// </summary>
-        public int BufferSize
-        {
-            get => this.bufferSize;
-            set
-            {
-                if (value < 0)
-                {
-                    this.bufferSize = -1;
-                    return;
-                }
-
-                this.bufferSize = value;
-                this.FixSize();
-            }
-        }
-
-        public virtual event NotifyCollectionChangedEventHandler? CollectionChanged;
-
-        public virtual event PropertyChangedEventHandler? PropertyChanged;
-
-        /// <summary>
-        ///     Ensures that the queue does not exceed the buffer size, if there is one.
-        /// </summary>
-        private void FixSize()
-        {
-            if (this.BufferSize > -1)
-            {
-                while (this.Count > this.BufferSize)
-                {
-                    this.Dequeue();
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Triggers a CollectionChanged event.
-        /// </summary>
-        /// <param name="action"> The action that has taken place. </param>
-        /// <param name="item"> The item that changed. </param>
-        /// <param name="index"> The index where the item was when it changed. </param>
-        protected virtual void OnCollectionChanged(NotifyCollectionChangedAction action, T item, int index)
-        {
-            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(
-                action
-                , item
-                , index)
-            );
-
-            OnPropertyChanged(nameof(Count));
-        }
-
-        /// <summary>
-        ///     Triggers a PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName"> The name of the property that changed. </param>
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <inheritdoc />
-        public virtual new void Clear()
-        {
-            base.Clear();
-            this.OnCollectionChanged(NotifyCollectionChangedAction.Reset, default!, -1);
-        }
-
-        /// <inheritdoc />
-        public virtual new T Dequeue()
-        {
-            var item = base.Dequeue();
-            this.OnCollectionChanged(NotifyCollectionChangedAction.Remove, item, this.Count);
-            return item;
-        }
-
-        /// <inheritdoc />
-        public virtual new void Enqueue(T item)
-        {
-            base.Enqueue(item);
-            this.OnCollectionChanged(NotifyCollectionChangedAction.Add, item, 0);
-            this.FixSize();
-        }
-
-        /// <summary>
-        ///     An alias for Dequeue.
-        /// </summary>
-        /// <returns> The item that was removed. </returns>
-        public T Pop()
-        {
-            return this.Dequeue();
-        }
-
-        /// <summary>
-        ///     An alias for Enqueue.
-        /// </summary>
-        /// <param name="item"> </param>
-        public void Push(T item)
-        {
-            this.Enqueue(item);
-        }
-    }
-
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -189,7 +37,9 @@ namespace Xfixy.WinUI
     {
         private AppWindow _appWindow;
         private string messageInp;
-        public ObservableQueue<Message> OutputItems { get; set; } = new ObservableQueue<Message>();
+        // TODO: Look for a way to implement a "inverted" ListView.
+        // https://github.com/AvaloniaUI/Avalonia/discussions/7596 (Didn't work)
+        public ObservableCollection<Message> OutputItems { get; set; } = new();
         #region IObserver
 
         private IDisposable unsubscriber;
@@ -213,6 +63,7 @@ namespace Xfixy.WinUI
 
         public void OnNext(string value)
         {
+            // https://devblogs.microsoft.com/oldnewthing/20190328-00/?p=102368
             Console.WriteLine("The current message is {0}", value);
             DispatcherQueue.TryEnqueue(() =>
             {
@@ -233,39 +84,42 @@ namespace Xfixy.WinUI
 
             var app = (App)Application.Current;
 
-            //OutputItems.Push(new("Test 1", DateTime.Now, HorizontalAlignment.Left));
-            //OutputItems.Push(new("Test 2", DateTime.Now, HorizontalAlignment.Left));
+#if DEBUG
+            OutputItems.Add(new("Test m1", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m2", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m3", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m4", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m5", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m6", DateTime.Now, HorizontalAlignment.Left));
+            OutputItems.Add(new("Test m7", DateTime.Now, HorizontalAlignment.Left));
+#endif
             Subscribe(app.Xfixy.OnMessage);
 
             _appWindow = GetAppWindowForCurrentWindow();
-            _appWindow.Closing += OnClosing;
-
+            _appWindow.Closing += OnClosing; // Unsubscribe
         }
 
         private void AddItemToEnd()
         {
-            //OutputItems.Push(
-            //    new Message(messageInp, DateTime.Now, HorizontalAlignment.Left)
-            //    );
-            InvertedListView.Items.Add(
+            OutputItems.Add(
                 new Message(messageInp, DateTime.Now, HorizontalAlignment.Left)
                 );
+            //InvertedListView.Items.Add(
+            //    new Message(messageInp, DateTime.Now, HorizontalAlignment.Left)
+            //    );
         }
 
-        private async void OnClosing(object sender, AppWindowClosingEventArgs e)
+        private void OnClosing(object sender, AppWindowClosingEventArgs e)
         {
-            // https://stackoverflow.com/questions/73697961/how-to-save-data-on-application-close-or-entered-background-in-winui3-app
-            e.Cancel = true;
+            //e.Cancel = true;
             this.Unsubscribe();
 
-            var app = (App)Application.Current;
-            app.WorkerCancellationTokenSource?.Cancel();
-            await Task.Delay(100);
+            //var app = (App)Application.Current;
+            //app.WorkerCancellationTokenSource?.Cancel();
+            //await Task.Delay(100);
 
-            this.Close();
+            //this.Close();
         }
-
-
         private AppWindow GetAppWindowForCurrentWindow()
         {
             IntPtr hWnd = WindowNative.GetWindowHandle(this);
